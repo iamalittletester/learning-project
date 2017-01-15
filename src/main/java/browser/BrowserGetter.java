6b4218a886beba1dc5347a9060a8567ecbc67327
iamalittletester <capreoara6@gmail.com>
2017-01-15 16:04:31 +0200

    DESC: First commit. Project structure and first set of tests.

---
+++ src/main/java/browser/BrowserGetter.java
@@ -0,0 +1,37 @@
+package browser;
+
+import org.eclipse.jetty.io.RuntimeIOException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.SystemClock;
+
+import static java.lang.System.setProperty;
+
+public class BrowserGetter {
+
+    public WebDriver getChromeDriver() {
+        String osName = System.getProperty("os.name").toLowerCase();
+        /**
+         *          detect operating system
+         *          if it is not one of the predefined ones, just throw exception
+         *          only create a webDriver instance for a known operating system
+         */
+
+        if (!osName.contains("windows") && !osName.contains("linux") && !osName.contains("mac"))
+            throw new RuntimeException("Could not initialize browser due to unknown operating system!");
+
+        if (osName.contains("windows")) {
+            setProperty("webdriver.chrome.driver", "src/test/resources/browserBinaries/chromedriver.exe");
+        }
+        if (osName.contains("linux")) {
+            setProperty("webdriver.chrome.driver", "src/test/resources/browserBinaries/chromedriver");
+        }
+        if (osName.contains("mac")) {
+            setProperty("webdriver.chrome.driver", "src/test/resources/browserBinaries/chromedriverMac");
+        }
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        return driver;
+    }
+}
