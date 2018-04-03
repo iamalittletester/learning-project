6db33dd8497810f2c84c8ac366107b508834b0c7
corina.pip <capreoara6@gmail.com>
2018-04-03 11:05:36 +0300

    Enabling thewaiter library.

---
+++ src/test/java/waitertests/WaiterTest.java
@@ -0,0 +1,31 @@
+package waitertests;
+
+import browser.BrowserGetter;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import waiter.Waiter;
+
+public class WaiterTest {
+    private Waiter waiter = new Waiter();
+    private WebDriver webDriver;
+    private BrowserGetter browserGetter = new BrowserGetter();
+
+    @BeforeClass
+    public void beforeClass() {
+        //initialize the Chrome browser here
+        webDriver = browserGetter.getChromeDriver();
+        //initialize page object class
+    }
+
+    @AfterClass
+    public void afterClass() {
+        webDriver.quit();
+    }
+
+    @Test
+    public void getTest() {
+        waiter.get("http://www.google.com", webDriver);
+    }
+}
