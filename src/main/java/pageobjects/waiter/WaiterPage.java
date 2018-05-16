aa7b28e60c52a7e0080fc8e43b276d8d9d2536a8
iamalittletester <capreoara6@gmail.com>
2018-05-16 19:44:50 +0300

    thewaiter tests: waiting for the Click event to be successful

--- src/main/java/pageobjects/waiter/WaiterPage.java
+++ src/main/java/pageobjects/waiter/WaiterPage.java
@@ -2,9 +2,18 @@ package pageobjects.waiter;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
+
+import static org.openqa.selenium.support.How.CSS;
 
 public class WaiterPage {
+    //for the Bing site
+    @FindBy(how = CSS, using = "input")
+    public WebElement bingSearchField;
 
-    @FindBy(how = How.CSS, using = "input") public WebElement bingSearchField;
+    //for the blog
+    @FindBy(how = CSS, using = ".search-toggle")
+    public WebElement searchButton;
+    @FindBy(how = CSS, using = "#primary-menu li:nth-child(2) a")
+    public WebElement menuLink;
+
 }
